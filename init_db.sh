24a8c8771b5da14b8a72a9bf7a847cb42b180e64
csum112 <cataremix15@gmail.com>
2020-05-11 05:56:44 +0300

    Added score calculator

--- init_db.sh
+++ init_db.sh
@@ -3,5 +3,6 @@
 cat /init_scripts/create_account.sql | sqlplus -s SYSTEM/oracle &&\
 cat /init_scripts/create_populate_tables.sql | sqlplus -s STUDENT/STUDENT &&\
 cat /init_scripts/gen_tests.sql | sqlplus -s STUDENT/STUDENT &&\
+cat /init_scripts/calculator.sql | sqlplus -s STUDENT/STUDENT &&\
 rm -r /init_scripts;
 
